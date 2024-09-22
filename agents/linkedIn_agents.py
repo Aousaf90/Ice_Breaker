31871e87179d440dd34fb83c36085622b23bd0ac
Aousaf Sulaman <59832341+Aousaf90@users.noreply.github.com>
2024-09-22 16:03:59 +0500

    module 3 lecture 5 completed

--- agents/linkedIn_agents.py
+++ agents/linkedIn_agents.py
@@ -8,9 +8,46 @@ from langchain.agents import (
 from langchain import hub
 import os
 import dotenv
-
+from langchain_community.tools.tavily_search import TavilySearchResults
 dotenv.load_dotenv()
 
 
+def get_tavily_linkedIn_url(name: str):
+    tavily_serach = TavilySearchResults()
+    result = tavily_serach.run(name)
+    return result
+
 def lookup(name: str)-> str:
-    return  "Hello World"
+    try:
+        llm = ChatOpenAI(
+            model= "gpt-3.5-turbo",
+            api_key=os.environ['OPENAI_API_KEY'],
+            temperature=0
+        )
+        tempplate =  """given the full name {name_of_person} I want you to get it me a link to there linkedin profile page
+                    Your answer should only contain only the URL"""
+        prompt_template = PromptTemplate(template=tempplate, input_variables=["name_of_person"])
+        tool_for_agents = [
+            Tool(
+                name= "Crowl google 4 linkedin profile page",
+                func=get_tavily_linkedIn_url,
+                description="useful when you need get the linkedin profile page"
+            )
+        ]
+        react_prompt = hub.pull("hwchase17/react")
+        agent = create_react_agent(tools=tool_for_agents, llm=llm, prompt=react_prompt)
+        agent_executor = AgentExecutor(verbose=True, agent=agent, tools=tool_for_agents)
+        result = agent_executor.invoke(
+            input={"input": prompt_template.format_prompt(name_of_person=name)}
+        )
+        linked_in_profile_url = result['output']
+        return linked_in_profile_url
+    except Exception as e:
+        print(f"Error: {e}")
+        return ""
+
+
+if __name__ == '__main__':
+    name = input("Entr your name: ")
+    result = lookup(name)
+    print(f"Result: {result}")
